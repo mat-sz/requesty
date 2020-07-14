d042cb9cde7e850c966cf83d2c1cce933de1f67e
Mat Sz <contact@matsz.dev>
2020-07-14 18:24:53 +0200

    updated build script

--- build.ts
+++ build.ts
@@ -1,40 +1,63 @@
 import { resolve } from 'path';
 import { removeSync, mkdirpSync, copySync, moveSync } from 'fs-extra';
 import { sync as spawnSync } from 'cross-spawn';
+import 'colors';
 
 const BUILD_DIR = resolve('./build');
 
-console.log('Building page...');
-spawnSync(resolve('./node_modules/.bin/react-scripts'), ['build'], {
-  env: {
-    ...process.env,
-    INLINE_RUNTIME_CHUNK: 'false',
-    GENERATE_SOURCEMAP: 'false',
-  },
-  cwd: resolve('./page'),
-  stdio: 'inherit',
-  shell: true,
-});
+const logStep = (step: string) => console.log(('> ' + step).blue);
 
-console.log('Creating a build directory...');
+const buildReactSubproject = (name: string) => {
+  logStep(name + ': Building React subproject...');
+  spawnSync(resolve('./node_modules/.bin/react-scripts'), ['build'], {
+    env: {
+      ...process.env,
+      INLINE_RUNTIME_CHUNK: 'false',
+      GENERATE_SOURCEMAP: 'false',
+    },
+    cwd: resolve('./', name),
+    stdio: 'inherit',
+    shell: true,
+  });
+};
+
+const copyBuildFiles = (name: string) => {
+  logStep(name + ': Copying build files...');
+  const targetDirectory = resolve(BUILD_DIR, name);
+
+  mkdirpSync(targetDirectory);
+
+  copySync(resolve('./', name, 'build'), targetDirectory, {
+    overwrite: true,
+    recursive: true,
+  });
+};
+
+const subprojects = [{ name: 'page', type: 'react' }];
+
+logStep('Removing build directory...');
 try {
   removeSync(BUILD_DIR);
 } catch {}
 
+logStep('Creating a build directory...');
 try {
   mkdirpSync(BUILD_DIR);
 } catch {}
 
-console.log('Copying build files...');
-copySync(resolve('./page/build'), BUILD_DIR, {
-  overwrite: true,
-  recursive: true,
-});
+for (let subproject of subprojects) {
+  switch (subproject.type) {
+    case 'react':
+      buildReactSubproject(subproject.name);
+      break;
+    default:
+      throw new Error('Unsupported subproject type: ' + subproject.type);
+  }
 
-console.log('Updating filenames...');
-moveSync(resolve(BUILD_DIR, 'index.html'), resolve(BUILD_DIR, 'page.html'));
+  copyBuildFiles(subproject.name);
+}
 
-console.log('Copying public files...');
+logStep('Copying public files...');
 copySync(resolve('./public'), BUILD_DIR, {
   overwrite: true,
   recursive: true,
